e02641640c644bc37d15d4b8c1a9ea0e59ee7d83
[ 大鳄 ] Asew <clair.judicael@gmail.com>
2020-10-11 12:04:52 +0100

    Added normal coordinates for shadow mapping
    Signed-off-by: [ 大鳄 ] Asew <clair.judicael@gmail.com>

--- src/simulator/views/panda/cube_mesh_generator.py
+++ src/simulator/views/panda/cube_mesh_generator.py
@@ -1,11 +1,16 @@
 from panda3d.core import Texture, GeomNode
 from panda3d.core import GeomVertexFormat, GeomVertexData
 from panda3d.core import Geom, GeomTriangles, GeomVertexWriter
-from panda3d.core import Vec3, Vec4, Point3
+from panda3d.core import LVector3, Vec3, Vec4, Point3
 from numbers import Real
 
 assert(__name__ != "__main__")
  
+def normalise(*args):
+    v = LVector3(*args)
+    v.normalize()
+    return v
+
 class CubeMeshGenerator():
     name : str
     mesh : Geom
@@ -14,19 +19,18 @@ class CubeMeshGenerator():
         self.name = name
         self.__finished = False
  
-        self.__format = GeomVertexFormat.getV3c4t2()
+        self.__format = GeomVertexFormat.getV3n3c4t2()
 
-        self.__vertex_data = GeomVertexData(name, self.__format, Geom.UHStatic)
-        self.__vertex_data.setNumRows(26136)
+        self.__vertex_data = GeomVertexData(name, self.__format, Geom.UHDynamic)
         
         self.mesh = Geom(self.__vertex_data)
-        self.__triangles = GeomTriangles(Geom.UHStatic)
+        self.__triangles = GeomTriangles(Geom.UHDynamic)
         self.__triangle_data = self.__triangles.modifyVertices()
-        self.__triangle_data.setNumRows(26136)
         
         self.__vertex = GeomVertexWriter(self.__vertex_data, 'vertex')
         self.__normal = GeomVertexWriter(self.__vertex_data, 'normal')
         self.__colour = GeomVertexWriter(self.__vertex_data, 'color')
+        self.__texcoord = GeomVertexWriter(self.__vertex_data, 'texcoord')
         
         self.__face_count = 0
     
@@ -40,17 +44,32 @@ class CubeMeshGenerator():
             self.__vertex.addData3f(x2, y2, z1)
             self.__vertex.addData3f(x2, y2, z2)
             self.__vertex.addData3f(x1, y1, z2)
+
+            self.__normal.addData3(normalise(2 * x1 - 1, 2 * y1 - 1, 2 * z1 - 1))
+            self.__normal.addData3(normalise(2 * x2 - 1, 2 * y2 - 1, 2 * z1 - 1))
+            self.__normal.addData3(normalise(2 * x2 - 1, 2 * y2 - 1, 2 * z2 - 1))
+            self.__normal.addData3(normalise(2 * x1 - 1, 2 * y1 - 1, 2 * z2 - 1))
         else:
             self.__vertex.addData3f(x1, y1, z1)
             self.__vertex.addData3f(x2, y1, z1)
             self.__vertex.addData3f(x2, y2, z2)
             self.__vertex.addData3f(x1, y2, z2)
 
+            self.__normal.addData3(normalise(2 * x1 - 1, 2 * y1 - 1, 2 * z1 - 1))
+            self.__normal.addData3(normalise(2 * x2 - 1, 2 * y1 - 1, 2 * z1 - 1))
+            self.__normal.addData3(normalise(2 * x2 - 1, 2 * y2 - 1, 2 * z2 - 1))
+            self.__normal.addData3(normalise(2 * x1 - 1, 2 * y2 - 1, 2 * z2 - 1))
+
         r, g, b = (colour, colour, colour) if isinstance(colour, Real) else colour
         self.__colour.addData4f(r, g, b, 1.0)
         self.__colour.addData4f(r, g, b, 1.0)
         self.__colour.addData4f(r, g, b, 1.0)
         self.__colour.addData4f(r, g, b, 1.0)
+
+        self.__texcoord.addData2f(0.0, 1.0)
+        self.__texcoord.addData2f(0.0, 0.0)
+        self.__texcoord.addData2f(1.0, 0.0)
+        self.__texcoord.addData2f(1.0, 1.0)
         
         vertex_id = self.__face_count * 4
         
