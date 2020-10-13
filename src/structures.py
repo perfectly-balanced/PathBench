0e3960d608d94d4f9135c4e0d006a9d225afa460
[ 大鳄 ] Asew <8472896+judicaelclair@users.noreply.github.com>
2020-10-13 01:08:41 +0100

    Revert "Rewrote point class (#1)" (#4)
    This reverts commit 9c2c7c740f0365f6a76bda7c613c0be8cdabb7ab.
    
    Co-authored-by: Nabushika <65562885+Nabushika@users.noreply.github.com>

--- src/structures.py
+++ src/structures.py
@@ -1,68 +1,22 @@
-from typing import Tuple, NamedTuple
+from typing import NamedTuple
+
 import torch
 
-class Point(torch.Tensor):
+
+class Point(NamedTuple, torch.Tensor):
     """
-    A point has a variable number of coordinates specified in the constructor.
-    Each point is immutable.
+    A point has 2 coordinates x and y.
     """
-    pos: Tuple[int, ...]
-
-    def __new__(cls, *ords, **kwargs):
-        return torch.Tensor.__new__(cls, len(ords))
-
-    def __init__(self, *ords, **kwargs):
-        if ("x" in kwargs and "y" in kwargs):
-            ords = (kwargs["x"], kwargs["y"])
-        assert (len(ords) > 1), "Needs at least two dimensions"
-        super().__init__()
-        self.pos = tuple(ords)
-        self.data = torch.FloatTensor(self.pos)
-    
-    @property
-    def x(self) -> int:
-        return self.pos[0]
-
-    @property
-    def y(self) -> int:
-        return self.pos[1]
-
-    @property
-    def z(self) -> int:
-        assert len(self.pos) > 2, "Point has no Z-coordinate"
-        return self.pos[2]
+    x: int
+    y: int
 
     def to_tensor(self) -> torch.Tensor:
-        return torch.Tensor([float(c) for c in self.pos])
+        return torch.Tensor([float(self.x), float(self.y)])
 
     @staticmethod
     def from_tensor(inp: torch.Tensor) -> 'Point':
-        assert len(list(inp.size())) == 1
-        return Point([int(torch.round(inp[i])) for i in range(list(inp.size())[0])])
+        return Point(int(torch.round(inp[0])), int(torch.round(inp[1])))
 
-    def __eq__(self, other: object) -> bool:
-        return isinstance(other, Point) and (self.pos == other.pos)
-
-    def __ne__(self, other: object) -> bool:
-        return not (self == other)
-
-    def __lt__(self, other: object) -> bool:
-        assert isinstance(other, Point), "Must compare with another point"
-        return self.pos < other.pos
-
-    def __hash__(self) -> int:
-        return hash(self.pos)
-
-    def __repr__(self) -> str:
-        if len(self.pos) == 2:
-            return f"Point(x={self.x}, y={self.y})"
-        return f"Point({', '.join(str(i) for i in self.pos)})"
-
-    def __copy__(self) -> 'Point':
-        return copy.deepcopy(self)
-
-    def __deepcopy__(self, memo: dict) -> 'Point':
-        return Point(*self.pos)
 
 class Size(NamedTuple):
     """
