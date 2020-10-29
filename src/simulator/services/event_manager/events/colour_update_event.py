3efb8c787517b1c81f3204a51d667820f8a0c3ec
[ 大鳄 ] Asew <clair.judicael@gmail.com>
2020-10-29 15:53:47 +0000

    Corrected name for ColourUpdateEvent
    Signed-off-by: [ 大鳄 ] Asew <clair.judicael@gmail.com>

--- src/simulator/services/event_manager/events/colour_update_event.py
+++ src/simulator/services/event_manager/events/colour_update_event.py
@@ -10,7 +10,7 @@ class ColourUpdateEvent(Event):
     __view: 'PersistentStateView'
 
     def __init__(self, colour: DynamicColour, view: 'PersistentStateView') -> None:
-        super().__init__("KeyFrame event")
+        super().__init__("ColourUpdate event")
         self.__colour = colour
         self.__view = view
 
