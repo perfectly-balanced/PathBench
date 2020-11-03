a51d34d38159186a7dd3768330793b756551f137
Abel Shields <admin@nabushika.com>
2020-11-03 17:53:43 +0000

    Made run_tests actually work

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -2,12 +2,13 @@ import sys, os
 import unittest
 from unittest import TestLoader, TestSuite, TextTestRunner, TestResult
 
-src_path = os.path.dirname(os.path.abspath(__file__)) + "/../src"
+tests_path = os.path.dirname(os.path.abspath(__file__))
+src_path = tests_path + "/../src"
 sys.path.append(src_path)
 
 if __name__ == '__main__':
     loader: TestLoader = unittest.TestLoader()
-    tests: TestSuite = loader.discover('.')
+    tests: TestSuite = loader.discover(tests_path)
     testRunner: TextTestRunner = unittest.TextTestRunner()
     res: TestResult = testRunner.run(tests)
     sys.exit(0 if (len(res.errors) == 0 and len(res.failures) == 0) else 1)
