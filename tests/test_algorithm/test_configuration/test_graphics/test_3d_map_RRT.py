a0d0cabd9dd60eaefb55e2389c2e539fd73dfaa4
Radina Milenkova <radinagm@gmail.com>
2020-11-25 03:12:23 +0200

    Setup done

---
+++ tests/test_algorithm/test_configuration/test_graphics/test_3d_map_RRT.py
@@ -0,0 +1,40 @@
+import time
+import pyautogui
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))), "src"))
+from constants import DATA_PATH
+
+pyautogui.click(1003, 218)
+
+# Pick 3d cube with RRT
+x, y= pyautogui.locateCenterOnScreen(os.path.join(DATA_PATH, 'Map.png'), confidence=0.5)
+pyautogui.click(x+160, y+5)
+x, y= pyautogui.locateCenterOnScreen(os.path.join(DATA_PATH,'3dCube.png'), confidence=0.9)
+pyautogui.click(x, y)
+x, y= pyautogui.locateCenterOnScreen(os.path.join(DATA_PATH,'Algorithm.png'), confidence=0.5)
+pyautogui.click(x+150, y)
+x, y= pyautogui.locateCenterOnScreen(os.path.join(DATA_PATH, 'rrt2.png'), confidence=0.9)
+pyautogui.click(x, y)
+x, y= pyautogui.locateCenterOnScreen(os.path.join(DATA_PATH, 'update.png'), confidence=0.5)
+pyautogui.click(x, y)
+time.sleep(5)
+
+# rotate map to pick a pos
+pyautogui.press('a', presses=5500)
+time.sleep(0.5)
+pyautogui.rightClick(997, 532)
+
+# make traversibles transparent for the RRT
+x, y= pyautogui.locateCenterOnScreen(os.path.join(DATA_PATH,'traversibles.png'), confidence=0.5)
+pyautogui.click(x-120, y)
+
+# #run algo
+pyautogui.press('t')
+time.sleep(1)
+
+# take ss
+pyautogui.press('o')
+time.sleep(1)
+pyautogui.press('p')
+
---
+++ tests/test_algorithm/test_configuration/test_graphics/test_labyrinth_with_A.py
@@ -0,0 +1,49 @@
+import time
+import pyautogui
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))), "src"))
+from constants import DATA_PATH
+
+
+pyautogui.click(1003, 218)
+
+
+# Select map Labyrinth, A* algorithm, update
+x, y= pyautogui.locateCenterOnScreen(os.path.join(DATA_PATH, 'Map.png'), confidence=0.5)
+pyautogui.click(x+160, y+5)
+x, y= pyautogui.locateCenterOnScreen(os.path.join(DATA_PATH, 'Labyrinth.png'), confidence=0.5)
+pyautogui.click(x, y)
+x, y= pyautogui.locateCenterOnScreen(os.path.join(DATA_PATH, 'Algorithm.png'), confidence=0.5)
+pyautogui.click(x+150, y)
+x, y= pyautogui.locateCenterOnScreen(os.path.join(DATA_PATH, 'A.png'), confidence=0.5)
+pyautogui.click(x, y)
+x, y= pyautogui.locateCenterOnScreen(os.path.join(DATA_PATH, 'update.png'), confidence=0.5)
+pyautogui.click(x, y)
+pyautogui.press('w', presses=5500)
+
+# pick colours and other modifications of the map
+x, y= pyautogui.locateCenterOnScreen(os.path.join(DATA_PATH, 'traversibles.png'), confidence=0.5)
+pyautogui.click(x-78, y)
+x, y= pyautogui.locateCenterOnScreen(os.path.join(DATA_PATH,'lightblue.png'), confidence=0.9)
+pyautogui.click(x, y)
+
+
+# start and end goals
+pyautogui.rightClick(900, 964)
+time.sleep(0.5)
+pyautogui.click(1511, 815)
+time.sleep(1)
+
+pyautogui.press('t')
+time.sleep(1)
+
+# take texture ss
+pyautogui.press('o')
+
+#make traversibles transparent for the other ss
+x, y= pyautogui.locateCenterOnScreen(os.path.join(DATA_PATH,'traversibles.png'), confidence=0.5)
+pyautogui.click(x-120, y)
+
+pyautogui.press('p')
+
