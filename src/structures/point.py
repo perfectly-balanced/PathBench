5a95483cda3676708035254f7408625c6a4cdd17
[ 大鳄 ] Asew <clair.judicael@gmail.com>
2020-12-20 14:56:20 +0000

    Deprecated Point.pos, changed to Point.values
    Signed-off-by: [ 大鳄 ] Asew <clair.judicael@gmail.com>

--- src/structures/point.py
+++ src/structures/point.py
@@ -1,4 +1,5 @@
 from typing import Union, Tuple
+from deprecated import deprecated
 
 import torch
 import copy
@@ -44,9 +45,13 @@ class Point(torch.Tensor):
     def n_dim(self) -> int:
         return len(self._values)
 
-    @property
     def values(self) -> Union[Tuple[float, ...], Tuple[int, ...]]:
         return self._values
+
+    @deprecated(reason="Use Point.values instead.")
+    @property
+    def pos(self) -> Union[Tuple[float, ...], Tuple[int, ...]]:
+        return self.values
 
     @property
     def is_float(self) -> bool:
