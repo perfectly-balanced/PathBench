1e04c4932c7dfb20d92f15c7fac5ce8023152bdf
[ 大鳄 ] Asew <clair.judicael@gmail.com>
2020-12-21 19:43:09 +0000

    Added ability to set fullscreen and window size, with fullscreen specifiable via env
    Signed-off-by: [ 大鳄 ] Asew <clair.judicael@gmail.com>

--- src/simulator/services/graphics/window.py
+++ src/simulator/services/graphics/window.py
@@ -1,4 +1,5 @@
 from typing import Optional
+import os
 
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import WindowProperties, LVector2i
@@ -6,17 +7,49 @@ from panda3d.core import WindowProperties, LVector2i
 from structures import Size
 
 class Window(ShowBase):
-    def __init__(self, title: str, size: Optional[Size] = None) -> None:
+    def __init__(self, title: str) -> None:
         super().__init__()
 
         props = WindowProperties()
         props.set_title(title)
-        if size is not None:
-            props.set_size(LVector2i(*size))
         self.win.request_properties(props)
+
+        if "PATH_BENCH_FULLSCREEN" in os.environ:
+            self.fullscreen = True
 
     def quit(self) -> None:
         self.destroy()
 
     def update(self) -> None:
         self.taskMgr.step()
+
+    @property
+    def size(self) -> str:
+        return 'size'
+
+    @size.getter
+    def size(self) -> Size:
+        props = self.win.get_properties()
+        return Size(*props.size)
+
+    @size.setter
+    def size(self, value: Size) -> None:
+        props = WindowProperties()
+        props.size = LVector2i(*value)
+        self.win.request_properties(props)
+
+    @property
+    def fullscreen(self) -> str:
+        return 'fullscreen'
+
+    @fullscreen.getter
+    def fullscreen(self) -> bool:
+        props = self.win.get_properties()
+        return props.fullscreen
+
+    @fullscreen.setter
+    def fullscreen(self, value: bool) -> None:
+        props = WindowProperties()
+        props.fullscreen = value
+        props.size = LVector2i(self.pipe.getDisplayWidth(), self.pipe.getDisplayHeight())
+        self.win.request_properties(props)
