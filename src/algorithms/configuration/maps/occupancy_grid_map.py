c56d5e71ec975f8aa0df015498b4974b953a2107
[ 大鳄 ] Asew <clair.judicael@gmail.com>
2020-12-23 14:23:24 +0000

    Implemented map update
    Signed-off-by: [ 大鳄 ] Asew <clair.judicael@gmail.com>

--- src/algorithms/configuration/maps/occupancy_grid_map.py
+++ src/algorithms/configuration/maps/occupancy_grid_map.py
@@ -11,6 +11,7 @@ from numbers import Real
 import copy
 
 from simulator.services.services import Services
+from simulator.services.event_manager.events.map_update_event import MapUpdateEvent
 from algorithms.configuration.maps.map import Map
 from algorithms.configuration.maps.dense_map import DenseMap
 from algorithms.configuration.entities.agent import Agent
@@ -37,7 +38,7 @@ class OccupancyGridMap(DenseMap):
         self.agent = agent
         self.goal = goal
         self.weight_grid = None
-        self.traversable_threshold = self.DEFAULT_TRAVERSABLE_THRESHOLD
+        self.traversable_threshold = None
         if weight_grid is not None:
             self.set_grid(weight_grid, weight_bounds, traversable_threshold, unmapped_value)
 
@@ -45,52 +46,120 @@ class OccupancyGridMap(DenseMap):
     def weight_bounds(self):
         return (0, 1)
 
+    @staticmethod
+    def __normalise(weight_bounds, x):
+        y = (weight_bounds[1] - weight_bounds[0])
+        return max(weight_bounds[0], min(weight_bounds[1], ((x - weight_bounds[0]) / y) if y else weight_bounds[0]))
+
     def set_grid(self, weight_grid: List[Any], weight_bounds: Optional[Tuple[Real, Real]] = None, traversable_threshold: Optional[Real] = None, unmapped_value: Optional[Real] = None) -> None:
         if not isinstance(weight_grid, np.ndarray):
             self.set_grid(np.array(weight_grid), weight_bounds, traversable_threshold, unmapped_value)
             return
+
+        (self.__new_grid if self.size is None else self.__update_grid)(weight_grid, weight_bounds, traversable_threshold, unmapped_value)
+
+
+    def __update_grid(self, weight_grid: np.ndarray, weight_bounds: Optional[Tuple[Real, Real]] = None, traversable_threshold: Optional[Real] = None, unmapped_value: Optional[Real] = None) -> None:
+        assert weight_grid.shape == self.weight_grid.shape, "Dimension mismatch in grid update."
+
+        def remove_obstacle(p: Point):
+            for i in range(len(self.obstacles)):
+                if self.obstacles[i].position == p:
+                    self.obstacles.pop(i)
+                    break
+
+        # bounds
+        if weight_bounds is None:
+            ignored = [] if unmapped_value is None else [unmapped_value]
+            weight_bounds = (min(flatten(weight_grid, ignored)), max(flatten(weight_grid, ignored)))
         
-        self.size = Size(*weight_grid.shape)
-        self.grid = np.full(self.size, self.CLEAR_ID, dtype=np.uint8)
-        self.weight_grid = np.empty(self.size, dtype=np.float32)
+        # threshold
+        if traversable_threshold is None:
+            traversable_threshold = min(weight_bounds[0] + (weight_bounds[1] - weight_bounds[0]) * self.DEFAULT_TRAVERSABLE_THRESHOLD, weight_bounds[1])
+        self.traversable_threshold = OccupancyGridMap.__normalise(weight_bounds, traversable_threshold)
+
+        updated_cells: List[Point] = []
+
+        # todo: when obstacle modified, extended wall update required
 
         # unmapped value
         if unmapped_value is not None:
             for idx in np.ndindex(*self.size):
-                if weight_grid[idx] == unmapped_value:
-                    self.grid[idx] = self.UNMAPPED_ID
+                if weight_grid[idx] == unmapped_value and self.grid[idx] != Map.UNMAPPED_ID:
+                    p = Point(*idx)
+                    if self.grid[idx] == Map.WALL_ID or self.grid[idx] == Map.EXTENDED_WALL_ID:
+                        remove_obstacle(p)
+                    self.grid[idx] = Map.UNMAPPED_ID
+                    updated_cells.append(p)
+        
+        # mapped values
+        for idx in np.ndindex(*self.size):
+            v = weight_grid[idx]
+            self.weight_grid[idx] = OccupancyGridMap.__normalise(weight_bounds, v)
+
+            if self.grid[idx] == Map.UNMAPPED_ID and weight_grid[idx] != unmapped_value:
+                p = Point(*idx)
+                updated_cells.append(p)
+
+                if v > traversable_threshold:
+                    self.grid[idx] = self.WALL_ID
+                    self.obstacles.append(Obstacle(p))
+                else:
+                    self.grid[idx] = self.CLEAR_ID
+            elif self.grid[idx] != Map.UNMAPPED_ID:
+                if v > traversable_threshold:
+                    if self.grid[idx] != Map.WALL_ID:
+                        p = Point(*idx)
+                        self.grid[idx] = self.WALL_ID
+                        self.obstacles.append(Obstacle(p))
+                        updated_cells.append(p)
+                elif self.grid[idx] == Map.WALL_ID:
+                    p = Point(*idx)
+                    self.grid[idx] = self.CLEAR_ID
+                    remove_obstacle(p)
+                    updated_cells.append(p)
+        
+        if self._services is not None:
+            self._services.ev_manager.post(MapUpdateEvent(updated_cells))
+
+
+    def __new_grid(self, weight_grid: np.ndarray, weight_bounds: Optional[Tuple[Real, Real]] = None, traversable_threshold: Optional[Real] = None, unmapped_value: Optional[Real] = None) -> None:
+        self.size = Size(*weight_grid.shape)
+        self.grid = np.full(self.size, self.CLEAR_ID, dtype=np.uint8)
+        self.weight_grid = np.empty(self.size, dtype=np.float32)
 
         # bounds
         if weight_bounds is None:
             ignored = [] if unmapped_value is None else [unmapped_value]
             weight_bounds = (min(flatten(weight_grid, ignored)), max(flatten(weight_grid, ignored)))
 
-        def normalise(x):
-            y = (weight_bounds[1] - weight_bounds[0])
-            return max(weight_bounds[0], min(weight_bounds[1], ((x - weight_bounds[0]) / y) if y else weight_bounds[0]))
-
         # threshold
         if traversable_threshold is None:
             traversable_threshold = min(weight_bounds[0] + (weight_bounds[1] - weight_bounds[0]) * self.DEFAULT_TRAVERSABLE_THRESHOLD, weight_bounds[1])
-        self.traversable_threshold = normalise(traversable_threshold)
+        self.traversable_threshold = OccupancyGridMap.__normalise(weight_bounds, traversable_threshold)
 
-        print(weight_bounds, unmapped_value)
-        print("start obstacles")
+        # unmapped value
+        if unmapped_value is not None:
+            for idx in np.ndindex(*self.size):
+                if weight_grid[idx] == unmapped_value:
+                    self.grid[idx] = self.UNMAPPED_ID
+        
+        # obstacles
         self.obstacles.clear()
         for idx in np.ndindex(*self.size):
             v = weight_grid[idx]
-            self.weight_grid[idx] = normalise(v)
-            if v >= traversable_threshold and self.grid[idx] != self.UNMAPPED_ID:
+            self.weight_grid[idx] = OccupancyGridMap.__normalise(weight_bounds, v)
+            if v > traversable_threshold and self.grid[idx] != self.UNMAPPED_ID:
                 self.grid[idx] = self.WALL_ID
                 self.obstacles.append(Obstacle(Point(*idx)))
-        print("end obstacles")
 
+        # entities
         self.grid[self.agent.position.values] = self.AGENT_ID
         self.grid[self.goal.position.values] = self.GOAL_ID
 
-        print("start extend walls")
+        # todo: the following works but very slow.
+        # Additionally, self.__update_grid() doesn't handle extended wall updates.
         # self.extend_walls()
-        print("end extend walls")
 
     def at(self, p: Point) -> int:
         return self.grid[p.values]
