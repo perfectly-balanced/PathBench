4f46f434f6867ba95ff686e516e12da22307058b
John <yaojohn6@gmail.com>
2020-12-27 01:13:52 -0800

    adding bootstrap full generate + train

--- src/algorithms/lstm/trainer.py
+++ src/algorithms/lstm/trainer.py
@@ -10,5 +10,8 @@ class Trainer:
         config: Dict[str, Any] = m.main_services.settings.trainer_custom_config
         if not config:
             config = m.main_services.settings.trainer_model.get_config()
+        #adding in-built map detection
+        config['training_data'] = ['_'.join(['training'] + m.main_services.settings.generator_labelling_atlases)]
+
         model = m.main_services.settings.trainer_model(m.main_services, config).to(m.main_services.torch.device)
         model.full_train_holdout()
