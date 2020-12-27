ab8b5c14d12453105542f2d89b2df67a84ddb469
Radina Milenkova <radinagm@gmail.com>
2020-12-27 23:36:53 +0200

    Adapt test_labytinth for virtual display

--- tests/test_algorithm/test_configuration/test_graphics/test_labyrinth_A.py
+++ tests/test_algorithm/test_configuration/test_graphics/test_labyrinth_A.py
@@ -24,22 +24,22 @@ pyautogui.click(x, y)
 
 
 # start and end goals coordinate input
-pyautogui.doubleClick(441, 629)
+pyautogui.click(342, 545)
 pyautogui.write('17')
-pyautogui.doubleClick(521, 626)
+pyautogui.doubleClick(422, 545)
 pyautogui.write('9')
-pyautogui.doubleClick(441, 696)
-pyautogui.write('2')
-pyautogui.doubleClick(521, 696)
+pyautogui.doubleClick(342, 617)
+pyautogui.write('6')
+pyautogui.doubleClick(422, 617)
 pyautogui.write('4')
 
 x, y = pyautogui.locateCenterOnScreen(os.path.join(TEST_DATA_PATH, 'update.png'), confidence=0.6)
 pyautogui.click(x, y)
-pyautogui.press('w', presses=5500)
+# pyautogui.press('w', presses=5000)
 
-
+time.sleep(2)
 # pick colours and other modifications of the map
-x, y = pyautogui.locateCenterOnScreen(os.path.join(TEST_DATA_PATH, 'traversables.png'), confidence=0.4)
+x, y = pyautogui.locateCenterOnScreen(os.path.join(TEST_DATA_PATH, 'traversables.png'), confidence=0.6)
 pyautogui.click(x - 78, y)
 x, y = pyautogui.locateCenterOnScreen(os.path.join(TEST_DATA_PATH, 'lightblue.png'), confidence=0.8)
 pyautogui.click(x, y)
@@ -51,7 +51,7 @@ time.sleep(0.5)
 pyautogui.press('o')
 
 # make traversables transparent
-x, y = pyautogui.locateCenterOnScreen(os.path.join(TEST_DATA_PATH, 'traversables.png'), confidence=0.4)
+x, y = pyautogui.locateCenterOnScreen(os.path.join(TEST_DATA_PATH, 'traversables.png'), confidence=0.6)
 pyautogui.click(x - 120, y)
 
 # get the latest taken screenshot
@@ -60,18 +60,15 @@ transparent_1 = max(list_of_ss, key=os.path.getctime)
 
 # take the second texture ss
 pyautogui.press('o')
-time.sleep(2)
+
+# wait until ss is saved
+time.sleep(3)
 
 # get latest screenshot
 list_of_ss = glob.glob(os.path.join(RESOURCES_PATH, 'screenshots/*.png'))
 transparent_2 = max(list_of_ss, key=os.path.getctime)
 
-# revert changes
-pyautogui.press('c')
-time.sleep(0.5)
-pyautogui.press('v')
-time.sleep(0.5)
-x, y = pyautogui.locateCenterOnScreen(os.path.join(TEST_DATA_PATH, 'traversables.png'), confidence=0.4)
+x, y = pyautogui.locateCenterOnScreen(os.path.join(TEST_DATA_PATH, 'traversables.png'), confidence=0.6)
 pyautogui.click(x - 120, y)
 
 # compare the 2 new screenshots with the expected ones
@@ -79,7 +76,9 @@ expected_transparent_1 = cv.imread(os.path.join(TEST_DATA_PATH, "labyrinth_A_1.p
 expected_transparent_2 = cv.imread(os.path.join(TEST_DATA_PATH, "labyrinth_A_2.png"))
 transparent_1 = cv.imread(transparent_1)
 transparent_2 = cv.imread(transparent_2)
+
 print(mse(expected_transparent_1, transparent_1))
+print(mse(expected_transparent_2, transparent_2))
 
 # Small error allowed for the top screen high res ss, usually very close to 0
 assert mse(expected_transparent_1, transparent_1) < 1
