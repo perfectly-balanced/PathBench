4673dcc2675dc38ef1cd26241d66948e5d272a9b
[ 大鳄 ] Asew <8472896+judicaelclair@users.noreply.github.com>
2020-12-30 22:56:43 +0000

    Polished file not found message

--- src/simulator/services/resources/directory.py
+++ src/simulator/services/resources/directory.py
@@ -68,8 +68,8 @@ class Directory(Service):
         if (isinstance(obj, np.ndarray) and np.prod(obj.shape) > 0) or obj: # disambiguates boolean check for numpy arrays
             self._services.debug.write("Loaded [{}]".format(self._full_path() + name), DebugLevel.LOW)
         else:
-            self._services.debug.write("File not found [{}]".format(self._full_path() + name) + "\n Consider deleting and re-generating",\
-                 DebugLevel.LOW)
+            msg = "File not found [{}]. Consider deleting and re-generating".format(self._full_path() + name)
+            self._services.debug.write(msg, DebugLevel.LOW)
         return obj
 
     def exists(self, name: str, extension: str) -> bool:
