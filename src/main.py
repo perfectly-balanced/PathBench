785e01e3fb47e03e04d426e0e813edacbf12a5cc
[ 大鳄 ] Asew <clair.judicael@gmail.com>
2020-12-30 22:17:50 +0000

    Fixed indentation
    Signed-off-by: [ 大鳄 ] Asew <clair.judicael@gmail.com>

--- src/main.py
+++ src/main.py
@@ -41,53 +41,53 @@ def configure_and_run(args) -> bool:
     if args.visualiser:
         config.load_simulator = True
         config.simulator_graphics = True
-    
+
     if args.trainer:
         config.trainer = True
 
-        if args.visualiser_flags is not None:
-            integral_pair_re = re.compile("\s*\(\s*[0-9]+\s*,\s*[0-9]+\s*\)\s*")
-            naked_integral_pair_re = re.compile("\s*[0-9]+\s*[0-9]+\s*")
+    if args.visualiser_flags is not None:
+        integral_pair_re = re.compile("\s*\(\s*[0-9]+\s*,\s*[0-9]+\s*\)\s*")
+        naked_integral_pair_re = re.compile("\s*[0-9]+\s*[0-9]+\s*")
 
-            data = ""
+        data = ""
 
-            def use_display_resolution():
-                nonlocal data
+        def use_display_resolution():
+            nonlocal data
 
-                for m in get_monitors():
-                    data += "win-size {} {}\n".format(m.width, m.height)
-                    break
+            for m in get_monitors():
+                data += "win-size {} {}\n".format(m.width, m.height)
+                break
 
-            for f in args.visualiser_flags:
-                if f.strip().lower() == "windowed-fullscreen":
-                    use_display_resolution()
-                    data += "win-origin 0 0\n"
-                    data += "undecorated true\n"
-                    continue
-
-                n, v = f.split("=")
-                n = n.strip().lower()
-                v = v.strip().lower()
-                if v in ("true", "1", "yes"):
-                    data += "{} true\n".format(n)
-                elif v in ("false", "0", "no"):
-                    data += "{} false\n".format(n)
-                elif integral_pair_re.match(v) is not None:
-                    v = re.sub('{\s+}|\(|\)', '', v)
-                    v1, v2 = v.split(',')
-                    data += "{} {} {}\n".format(n, v1, v2)
-                elif naked_integral_pair_re.match(v) is not None:
-                    v = v.strip()
-                    v = re.sub('\s+', ',', v)
-                    v1, v2 = v.split(',')
-                    data += "{} {} {}\n".format(n, v1, v2)
-                else:
-                    data += "{} {}".format(n, v)
-
-            if "fullscreen true" in data and "win-size" not in data:
+        for f in args.visualiser_flags:
+            if f.strip().lower() == "windowed-fullscreen":
                 use_display_resolution()
+                data += "win-origin 0 0\n"
+                data += "undecorated true\n"
+                continue
 
-            load_prc_file_data('', data)
+            n, v = f.split("=")
+            n = n.strip().lower()
+            v = v.strip().lower()
+            if v in ("true", "1", "yes"):
+                data += "{} true\n".format(n)
+            elif v in ("false", "0", "no"):
+                data += "{} false\n".format(n)
+            elif integral_pair_re.match(v) is not None:
+                v = re.sub('{\s+}|\(|\)', '', v)
+                v1, v2 = v.split(',')
+                data += "{} {} {}\n".format(n, v1, v2)
+            elif naked_integral_pair_re.match(v) is not None:
+                v = v.strip()
+                v = re.sub('\s+', ',', v)
+                v1, v2 = v.split(',')
+                data += "{} {} {}\n".format(n, v1, v2)
+            else:
+                data += "{} {}".format(n, v)
+
+        if "fullscreen true" in data and "win-size" not in data:
+            use_display_resolution()
+
+        load_prc_file_data('', data)
     elif args.visualiser_flags:
         raise ValueError("Visualiser isn't running, but flags were provided")
 
@@ -106,7 +106,7 @@ def configure_and_run(args) -> bool:
         config.simulator_write_debug_level = DebugLevel.HIGH
     else:
         raise NotImplementedError
-    
+
     mr = MainRunner(config)
     mr.run()
     return True
